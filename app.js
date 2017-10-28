4f5bcd0ecb100d06f66b22c1c359c3b17331aacc
Marina Nakagawa <mari10172001@yahoo.co.jp>
2017-10-28 12:56:10 +0000

    ランキングの実装

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ rl.on('close', () => {
 	const rankingArray = Array.from(map).sort((pair1, pair2) => {
 		return pair2[1].change - pair1[1].change;
 	});
-	console.log(rankingArray)
+	//連想配列のMapとは違うmap 各要素に関数を適用し新しい配列を作る
+	const rankingStrings = rankingArray.map((pair) => {
+		return pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + '変化率:' + pair[1].change;
+	});
+	console.log(rankingStrings)
 });
 
